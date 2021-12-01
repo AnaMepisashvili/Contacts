d7525361526064886ca291acbf9404c9c42cd1cb
AnaMepisashvili <85555767+AnaMepisashvili@users.noreply.github.com>
2021-12-01 11:07:36 +0400

    Initial Commit

---
+++ Contacts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Contacts
+//
+//  Created by Ana Mepisashvili on 01.12.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
